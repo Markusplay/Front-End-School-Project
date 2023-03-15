4f7ede8dfe30ac347c6a52cbd3d6462d9198ad9f
Markusplay <mshilo2004@gmail.com>
2023-03-16 00:35:18 +0200

    Added ESLINT

--- src/components/pages/404-page/NotFoundPage.tsx
+++ src/components/pages/404-page/NotFoundPage.tsx
@@ -1,19 +1,19 @@
-import Link from 'next/link';
-import styles from './NotFoundPage.module.scss';
-import { Button } from '@mui/material';
+import { Button } from "@mui/material";
+import Link from "next/link";
+
+import styles from "./NotFoundPage.module.scss";
 
 const NotFoundPage = () => (
-
-    <div className={styles['page-content']}>
-      <h5 className={styles['not-found-text']}>
-        Упс! 404 помилка. Сторінку не знайдено.
-      </h5>
-      <div className={styles['button']}>
-        <Link href={'/'}>
-          <Button variant='contained'>Повернутися на головну</Button>
-        </Link>
-      </div>
+  <div className={styles["page-content"]}>
+    <h5 className={styles["not-found-text"]}>
+      Упс! 404 помилка. Сторінку не знайдено.
+    </h5>
+    <div className={styles.button}>
+      <Link href={"/"}>
+        <Button variant="contained">Повернутися на головну</Button>
+      </Link>
     </div>
+  </div>
 );
 
 export default NotFoundPage;
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,4 +1,4 @@
-import NotFoundPage from '@/components/pages/404-page';
+import NotFoundPage from "@/components/pages/404-page";
 const NotFound = () => <NotFoundPage />;
 
 export default NotFound;
