2db5cf29ec5bbe20ee45894543eba6f472dcdacb
Markusplay <mshilo2004@gmail.com>
2023-03-15 23:40:08 +0200

    created porject

---
+++ src/components/pages/404-page/NotFoundPage.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import styles from './NotFoundPage.module.scss';
+import { Button } from '@mui/material';
+
+const NotFoundPage = () => (
+
+    <div className={styles['page-content']}>
+      <h5 className={styles['not-found-text']}>
+        Упс! 404 помилка. Сторінку не знайдено.
+      </h5>
+      <div className={styles['button']}>
+        <Link href={'/'}>
+          <Button variant='contained'>Повернутися на головну</Button>
+        </Link>
+      </div>
+    </div>
+);
+
+export default NotFoundPage;
